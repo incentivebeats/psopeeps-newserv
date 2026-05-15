d5df5eec5ac95a118ac5eb6e5725eb64c948f1f2
James Osborne <incentive@circlewithadot.net>
2026-05-15 19:39:21 -0400

    Add Dreamcast V2 EXP client patches

---
+++ system/client-functions/PsoPeepsV2EXP10xDC.s
@@ -0,0 +1,1076 @@
+.meta visibility="menu"
+.meta key="PsoPeepsV2EXP10xDC"
+.meta name="V2 10x"
+.meta description="Dreamcast V2 EXP"
+
+.versions 2OEF
+
+entry_ptr:
+reloc0:
+  .offsetof start
+start:
+  .include  WriteCodeBlocks
+
+  .align    4
+  .data     0x8CD21DBC
+  .data     2
+  .binary   0a00
+
+  .align    4
+  .data     0x8CD21DE0
+  .data     2
+  .binary   3c00
+
+  .align    4
+  .data     0x8CD21E04
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD21E28
+  .data     2
+  .binary   4600
+
+  .align    4
+  .data     0x8CD21E4C
+  .data     2
+  .binary   6400
+
+  .align    4
+  .data     0x8CD21E70
+  .data     2
+  .binary   dc05
+
+  .align    4
+  .data     0x8CD21E94
+  .data     2
+  .binary   c800
+
+  .align    4
+  .data     0x8CD21EB8
+  .data     2
+  .binary   a000
+
+  .align    4
+  .data     0x8CD21EDC
+  .data     2
+  .binary   a000
+
+  .align    4
+  .data     0x8CD21F00
+  .data     2
+  .binary   aa00
+
+  .align    4
+  .data     0x8CD21F24
+  .data     2
+  .binary   f000
+
+  .align    4
+  .data     0x8CD21F48
+  .data     2
+  .binary   2800
+
+  .align    4
+  .data     0x8CD21F6C
+  .data     2
+  .binary   2800
+
+  .align    4
+  .data     0x8CD21F90
+  .data     2
+  .binary   2c01
+
+  .align    4
+  .data     0x8CD21FB4
+  .data     2
+  .binary   1801
+
+  .align    4
+  .data     0x8CD21FD8
+  .data     2
+  .binary   581b
+
+  .align    4
+  .data     0x8CD21FFC
+  .data     2
+  .binary   6400
+
+  .align    4
+  .data     0x8CD22020
+  .data     2
+  .binary   6400
+
+  .align    4
+  .data     0x8CD22044
+  .data     2
+  .binary   ac0d
+
+  .align    4
+  .data     0x8CD22068
+  .data     2
+  .binary   c800
+
+  .align    4
+  .data     0x8CD2208C
+  .data     2
+  .binary   8813
+
+  .align    4
+  .data     0x8CD2211C
+  .data     2
+  .binary   2800
+
+  .align    4
+  .data     0x8CD22140
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD22164
+  .data     2
+  .binary   9600
+
+  .align    4
+  .data     0x8CD22188
+  .data     2
+  .binary   1e00
+
+  .align    4
+  .data     0x8CD221AC
+  .data     2
+  .binary   b400
+
+  .align    4
+  .data     0x8CD221D0
+  .data     2
+  .binary   dc00
+
+  .align    4
+  .data     0x8CD221F4
+  .data     2
+  .binary   c800
+
+  .align    4
+  .data     0x8CD22218
+  .data     2
+  .binary   f000
+
+  .align    4
+  .data     0x8CD2223C
+  .data     2
+  .binary   3c00
+
+  .align    4
+  .data     0x8CD222F0
+  .data     2
+  .binary   f82a
+
+  .align    4
+  .data     0x8CD2247C
+  .data     2
+  .binary   6400
+
+  .align    4
+  .data     0x8CD224A0
+  .data     2
+  .binary   3c00
+
+  .align    4
+  .data     0x8CD224C4
+  .data     2
+  .binary   2800
+
+  .align    4
+  .data     0x8CD224E8
+  .data     2
+  .binary   2800
+
+  .align    4
+  .data     0x8CD2250C
+  .data     2
+  .binary   dc05
+
+  .align    4
+  .data     0x8CD22530
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD22578
+  .data     2
+  .binary   983a
+
+  .align    4
+  .data     0x8CD225C0
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD227DC
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD22800
+  .data     2
+  .binary   6400
+
+  .align    4
+  .data     0x8CD22824
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD22848
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD2286C
+  .data     2
+  .binary   3c00
+
+  .align    4
+  .data     0x8CD22890
+  .data     2
+  .binary   4600
+
+  .align    4
+  .data     0x8CD228B4
+  .data     2
+  .binary   aa00
+
+  .align    4
+  .data     0x8CD228D8
+  .data     2
+  .binary   6400
+
+  .align    4
+  .data     0x8CD228FC
+  .data     2
+  .binary   7800
+
+  .align    4
+  .data     0x8CD22920
+  .data     2
+  .binary   8c00
+
+  .align    4
+  .data     0x8CD22944
+  .data     2
+  .binary   fa00
+
+  .align    4
+  .data     0x8CD22968
+  .data     2
+  .binary   dc00
+
+  .align    4
+  .data     0x8CD2298C
+  .data     2
+  .binary   f000
+
+  .align    4
+  .data     0x8CD229B0
+  .data     2
+  .binary   0401
+
+  .align    4
+  .data     0x8CD22B3C
+  .data     2
+  .binary   6801
+
+  .align    4
+  .data     0x8CD22B60
+  .data     2
+  .binary   ae01
+
+  .align    4
+  .data     0x8CD22B84
+  .data     2
+  .binary   a401
+
+  .align    4
+  .data     0x8CD22BA8
+  .data     2
+  .binary   c201
+
+  .align    4
+  .data     0x8CD22BCC
+  .data     2
+  .binary   ea01
+
+  .align    4
+  .data     0x8CD22BF0
+  .data     2
+  .binary   1e0a
+
+  .align    4
+  .data     0x8CD22C14
+  .data     2
+  .binary   8002
+
+  .align    4
+  .data     0x8CD22C38
+  .data     2
+  .binary   4402
+
+  .align    4
+  .data     0x8CD22C5C
+  .data     2
+  .binary   4402
+
+  .align    4
+  .data     0x8CD22C80
+  .data     2
+  .binary   5802
+
+  .align    4
+  .data     0x8CD22CA4
+  .data     2
+  .binary   bc02
+
+  .align    4
+  .data     0x8CD22CC8
+  .data     2
+  .binary   9001
+
+  .align    4
+  .data     0x8CD22CEC
+  .data     2
+  .binary   9001
+
+  .align    4
+  .data     0x8CD22D10
+  .data     2
+  .binary   7a03
+
+  .align    4
+  .data     0x8CD22D34
+  .data     2
+  .binary   f802
+
+  .align    4
+  .data     0x8CD22D58
+  .data     2
+  .binary   a861
+
+  .align    4
+  .data     0x8CD22D7C
+  .data     2
+  .binary   fa00
+
+  .align    4
+  .data     0x8CD22DA0
+  .data     2
+  .binary   2800
+
+  .align    4
+  .data     0x8CD22DC4
+  .data     2
+  .binary   204e
+
+  .align    4
+  .data     0x8CD22DE8
+  .data     2
+  .binary   8002
+
+  .align    4
+  .data     0x8CD22E0C
+  .data     2
+  .binary   8813
+
+  .align    4
+  .data     0x8CD22E9C
+  .data     2
+  .binary   9001
+
+  .align    4
+  .data     0x8CD22EC0
+  .data     2
+  .binary   3007
+
+  .align    4
+  .data     0x8CD22EE4
+  .data     2
+  .binary   3a02
+
+  .align    4
+  .data     0x8CD22F08
+  .data     2
+  .binary   6e00
+
+  .align    4
+  .data     0x8CD22F2C
+  .data     2
+  .binary   6202
+
+  .align    4
+  .data     0x8CD22F50
+  .data     2
+  .binary   9e02
+
+  .align    4
+  .data     0x8CD22F74
+  .data     2
+  .binary   8002
+
+  .align    4
+  .data     0x8CD22F98
+  .data     2
+  .binary   bc02
+
+  .align    4
+  .data     0x8CD22FBC
+  .data     2
+  .binary   ae01
+
+  .align    4
+  .data     0x8CD23070
+  .data     2
+  .binary   3075
+
+  .align    4
+  .data     0x8CD231FC
+  .data     2
+  .binary   ea01
+
+  .align    4
+  .data     0x8CD23220
+  .data     2
+  .binary   0401
+
+  .align    4
+  .data     0x8CD23244
+  .data     2
+  .binary   e600
+
+  .align    4
+  .data     0x8CD23268
+  .data     2
+  .binary   e600
+
+  .align    4
+  .data     0x8CD2328C
+  .data     2
+  .binary   1e0a
+
+  .align    4
+  .data     0x8CD232B0
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD2331C
+  .data     2
+  .binary   b888
+
+  .align    4
+  .data     0x8CD23340
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD2355C
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD23580
+  .data     2
+  .binary   ea01
+
+  .align    4
+  .data     0x8CD235A4
+  .data     2
+  .binary   3007
+
+  .align    4
+  .data     0x8CD235C8
+  .data     2
+  .binary   a401
+
+  .align    4
+  .data     0x8CD235EC
+  .data     2
+  .binary   ae01
+
+  .align    4
+  .data     0x8CD23610
+  .data     2
+  .binary   c201
+
+  .align    4
+  .data     0x8CD23634
+  .data     2
+  .binary   5802
+
+  .align    4
+  .data     0x8CD23658
+  .data     2
+  .binary   ea01
+
+  .align    4
+  .data     0x8CD2367C
+  .data     2
+  .binary   0802
+
+  .align    4
+  .data     0x8CD236A0
+  .data     2
+  .binary   2602
+
+  .align    4
+  .data     0x8CD236C4
+  .data     2
+  .binary   d002
+
+  .align    4
+  .data     0x8CD236E8
+  .data     2
+  .binary   9e02
+
+  .align    4
+  .data     0x8CD2370C
+  .data     2
+  .binary   bc02
+
+  .align    4
+  .data     0x8CD23730
+  .data     2
+  .binary   da02
+
+  .align    4
+  .data     0x8CD238BC
+  .data     2
+  .binary   3403
+
+  .align    4
+  .data     0x8CD238E0
+  .data     2
+  .binary   9803
+
+  .align    4
+  .data     0x8CD23904
+  .data     2
+  .binary   8403
+
+  .align    4
+  .data     0x8CD23928
+  .data     2
+  .binary   ac03
+
+  .align    4
+  .data     0x8CD2394C
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD23970
+  .data     2
+  .binary   d80e
+
+  .align    4
+  .data     0x8CD23994
+  .data     2
+  .binary   b004
+
+  .align    4
+  .data     0x8CD239B8
+  .data     2
+  .binary   6004
+
+  .align    4
+  .data     0x8CD239DC
+  .data     2
+  .binary   6004
+
+  .align    4
+  .data     0x8CD23A00
+  .data     2
+  .binary   7404
+
+  .align    4
+  .data     0x8CD23A24
+  .data     2
+  .binary   0005
+
+  .align    4
+  .data     0x8CD23A48
+  .data     2
+  .binary   7003
+
+  .align    4
+  .data     0x8CD23A6C
+  .data     2
+  .binary   7003
+
+  .align    4
+  .data     0x8CD23A90
+  .data     2
+  .binary   7805
+
+  .align    4
+  .data     0x8CD23AB4
+  .data     2
+  .binary   5005
+
+  .align    4
+  .data     0x8CD23AD8
+  .data     2
+  .binary   98b7
+
+  .align    4
+  .data     0x8CD23AFC
+  .data     2
+  .binary   2c01
+
+  .align    4
+  .data     0x8CD23B20
+  .data     2
+  .binary   5000
+
+  .align    4
+  .data     0x8CD23B44
+  .data     2
+  .binary   28a0
+
+  .align    4
+  .data     0x8CD23B68
+  .data     2
+  .binary   b004
+
+  .align    4
+  .data     0x8CD23B8C
+  .data     2
+  .binary   8813
+
+  .align    4
+  .data     0x8CD23C1C
+  .data     2
+  .binary   7003
+
+  .align    4
+  .data     0x8CD23C40
+  .data     2
+  .binary   f00a
+
+  .align    4
+  .data     0x8CD23C64
+  .data     2
+  .binary   4c04
+
+  .align    4
+  .data     0x8CD23C88
+  .data     2
+  .binary   dc00
+
+  .align    4
+  .data     0x8CD23CAC
+  .data     2
+  .binary   8804
+
+  .align    4
+  .data     0x8CD23CD0
+  .data     2
+  .binary   d804
+
+  .align    4
+  .data     0x8CD23CF4
+  .data     2
+  .binary   b004
+
+  .align    4
+  .data     0x8CD23D18
+  .data     2
+  .binary   0005
+
+  .align    4
+  .data     0x8CD23D3C
+  .data     2
+  .binary   9803
+
+  .align    4
+  .data     0x8CD23DF0
+  .data     2
+  .binary   08cf
+
+  .align    4
+  .data     0x8CD23F7C
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD23FA0
+  .data     2
+  .binary   0802
+
+  .align    4
+  .data     0x8CD23FC4
+  .data     2
+  .binary   e001
+
+  .align    4
+  .data     0x8CD23FE8
+  .data     2
+  .binary   e001
+
+  .align    4
+  .data     0x8CD2400C
+  .data     2
+  .binary   d80e
+
+  .align    4
+  .data     0x8CD24030
+  .data     2
+  .binary   5a00
+
+  .align    4
+  .data     0x8CD2409C
+  .data     2
+  .binary   78e6
+
+  .align    4
+  .data     0x8CD240C0
+  .data     2
+  .binary   5a00
+
+  .align    4
+  .data     0x8CD242DC
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD24300
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD24324
+  .data     2
+  .binary   f00a
+
+  .align    4
+  .data     0x8CD24348
+  .data     2
+  .binary   8403
+
+  .align    4
+  .data     0x8CD2436C
+  .data     2
+  .binary   9803
+
+  .align    4
+  .data     0x8CD24390
+  .data     2
+  .binary   ac03
+
+  .align    4
+  .data     0x8CD243B4
+  .data     2
+  .binary   7404
+
+  .align    4
+  .data     0x8CD243D8
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD243FC
+  .data     2
+  .binary   1004
+
+  .align    4
+  .data     0x8CD24420
+  .data     2
+  .binary   3804
+
+  .align    4
+  .data     0x8CD24444
+  .data     2
+  .binary   1405
+
+  .align    4
+  .data     0x8CD24468
+  .data     2
+  .binary   d804
+
+  .align    4
+  .data     0x8CD2448C
+  .data     2
+  .binary   0005
+
+  .align    4
+  .data     0x8CD244B0
+  .data     2
+  .binary   2805
+
+  .align    4
+  .data     0x8CD2463C
+  .data     2
+  .binary   7805
+
+  .align    4
+  .data     0x8CD24660
+  .data     2
+  .binary   500a
+
+  .align    4
+  .data     0x8CD24684
+  .data     2
+  .binary   280a
+
+  .align    4
+  .data     0x8CD246A8
+  .data     2
+  .binary   780a
+
+  .align    4
+  .data     0x8CD246CC
+  .data     2
+  .binary   f00a
+
+  .align    4
+  .data     0x8CD246F0
+  .data     2
+  .binary   d020
+
+  .align    4
+  .data     0x8CD24714
+  .data     2
+  .binary   800c
+
+  .align    4
+  .data     0x8CD24738
+  .data     2
+  .binary   e00b
+
+  .align    4
+  .data     0x8CD2475C
+  .data     2
+  .binary   e00b
+
+  .align    4
+  .data     0x8CD24780
+  .data     2
+  .binary   080c
+
+  .align    4
+  .data     0x8CD247A4
+  .data     2
+  .binary   200d
+
+  .align    4
+  .data     0x8CD247C8
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD247EC
+  .data     2
+  .binary   e803
+
+  .align    4
+  .data     0x8CD24810
+  .data     2
+  .binary   100e
+
+  .align    4
+  .data     0x8CD24834
+  .data     2
+  .binary   c00d
+
+  .align    4
+  .data     0x8CD24858
+  .data     2
+  .binary   ffff
+
+  .align    4
+  .data     0x8CD2487C
+  .data     2
+  .binary   5802
+
+  .align    4
+  .data     0x8CD248A0
+  .data     2
+  .binary   a000
+
+  .align    4
+  .data     0x8CD248C4
+  .data     2
+  .binary   e8fd
+
+  .align    4
+  .data     0x8CD248E8
+  .data     2
+  .binary   800c
+
+  .align    4
+  .data     0x8CD2490C
+  .data     2
+  .binary   8813
+
+  .align    4
+  .data     0x8CD2499C
+  .data     2
+  .binary   000a
+
+  .align    4
+  .data     0x8CD249C0
+  .data     2
+  .binary   0019
+
+  .align    4
+  .data     0x8CD249E4
+  .data     2
+  .binary   b80b
+
+  .align    4
+  .data     0x8CD24A08
+  .data     2
+  .binary   f401
+
+  .align    4
+  .data     0x8CD24A2C
+  .data     2
+  .binary   300c
+
+  .align    4
+  .data     0x8CD24A50
+  .data     2
+  .binary   d00c
+
+  .align    4
+  .data     0x8CD24A74
+  .data     2
+  .binary   800c
+
+  .align    4
+  .data     0x8CD24A98
+  .data     2
+  .binary   200d
+
+  .align    4
+  .data     0x8CD24ABC
+  .data     2
+  .binary   500a
+
+  .align    4
+  .data     0x8CD24B70
+  .data     2
+  .binary   ffff
+
+  .align    4
+  .data     0x8CD24CFC
+  .data     2
+  .binary   f00a
+
+  .align    4
+  .data     0x8CD24D20
+  .data     2
+  .binary   3007
+
+  .align    4
+  .data     0x8CD24D44
+  .data     2
+  .binary   e006
+
+  .align    4
+  .data     0x8CD24D68
+  .data     2
+  .binary   e006
+
+  .align    4
+  .data     0x8CD24D8C
+  .data     2
+  .binary   d020
+
+  .align    4
+  .data     0x8CD24DB0
+  .data     2
+  .binary   c800
+
+  .align    4
+  .data     0x8CD24E1C
+  .data     2
+  .binary   ffff
+
+  .align    4
+  .data     0x8CD24E40
+  .data     2
+  .binary   c800
+
+  .align    4
+  .data     0x8CD2505C
+  .data     2
+  .binary   3200
+
+  .align    4
+  .data     0x8CD25080
+  .data     2
+  .binary   f00a
+
+  .align    4
+  .data     0x8CD250A4
+  .data     2
+  .binary   0019
+
+  .align    4
+  .data     0x8CD250C8
+  .data     2
+  .binary   280a
+
+  .align    4
+  .data     0x8CD250EC
+  .data     2
+  .binary   500a
+
+  .align    4
+  .data     0x8CD25110
+  .data     2
+  .binary   780a
+
+  .align    4
+  .data     0x8CD25134
+  .data     2
+  .binary   080c
+
+  .align    4
+  .data     0x8CD25158
+  .data     2
+  .binary   f00a
+
+  .align    4
+  .data     0x8CD2517C
+  .data     2
+  .binary   400b
+
+  .align    4
+  .data     0x8CD251A0
+  .data     2
+  .binary   900b
+
+  .align    4
+  .data     0x8CD251C4
+  .data     2
+  .binary   480d
+
+  .align    4
+  .data     0x8CD251E8
+  .data     2
+  .binary   d00c
+
+  .align    4
+  .data     0x8CD2520C
+  .data     2
+  .binary   200d
+
+  .align    4
+  .data     0x8CD25230
+  .data     2
+  .binary   700d
+
+  .align    4
+  .data     0
+  .data     0
