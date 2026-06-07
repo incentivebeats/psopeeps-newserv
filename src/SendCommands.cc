235e883cedf5a1acb95107de1c3f2ea7601485c5
James Osborne <incentive@circlewithadot.net>
2026-06-07 16:53:41 -0400

    Do not use BB color tokens in game list names
    (cherry picked from commit b9cd17d9dc6a071f9d51ce89a2287a182d3f3fe9)

--- src/SendCommands.cc
+++ src/SendCommands.cc
@@ -1893,11 +1893,7 @@ void send_game_menu_t(std::shared_ptr<Client> c, bool is_spectator_team_list, bo
       }
     }
 
-    if ((c->version() == Version::BB_V4) && (l->brutal_peeps_tier >= 1)) {
-      e.name.encode("$C4" + l->name, c->language());
-    } else {
-      e.name.encode(l->name, c->language());
-    }
+    e.name.encode(l->name, c->language());
   }
 
   send_command_vt(c, is_spectator_team_list ? 0xE6 : 0x08, entries.size() - 1, entries);
